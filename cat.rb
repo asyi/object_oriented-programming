626d35d93d286cd37d2f094dfc3bdbd988e45136
Althea Yi <altheaserena.yi@gmail.com>
2016-06-02 14:18:43 -0400

    completed step 3

--- cat.rb
+++ cat.rb
@@ -1,7 +1,16 @@
 class Cat
 
-  def initalize(name, preferred_food, meal_time)
-    
-  end
+  attr_accessor :name, :preferred_food, :meal_time
 
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
 end
+
+crookshanks = Cat.new("Crookshanks", "fish", 14)
+kitty = Cat.new("Kitty", "cake", 6)
+cookie = Cat.new("Cookie", "cream", 9)
+
+puts cookie.inspect
