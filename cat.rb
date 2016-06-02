5dc3c998eb782c4faed20f4c7027f559e9305392
Althea Yi <altheaserena.yi@gmail.com>
2016-06-02 15:20:17 -0400

    finished meal times

--- cat.rb
+++ cat.rb
@@ -1,16 +1,27 @@
 class Cat
 
-  attr_accessor :name, :preferred_food, :meal_time
+  # attr_accessor :name, :preferred_food, :meal_time
 
-  def initialize(name, preferred_food, meal_time)
-    @name = name
-    @preferred_food = preferred_food
-    @meal_time = meal_time
+    def initialize(name, preferred_food, meal_time)
+      @name = name
+      @preferred_food = preferred_food
+      @meal_time = meal_time
+
+      def meow
+        puts "My name is #{@name} and I eat #{@preferred_food} at #{eats_at}."
+        self
+      end
+
+    def eats_at
+      if @meal_time > 12
+        return "#{@meal_time - 12}" + "PM"
+      else
+        return @meal_time.to_s + "AM"
+      end
+    endr
   end
 end
 
-crookshanks = Cat.new("Crookshanks", "fish", 14)
-kitty = Cat.new("Kitty", "cake", 6)
-cookie = Cat.new("Cookie", "cream", 9)
+crookshanks = Cat.new("Crookshanks", "fish", 14).meow
 
-puts cookie.inspect
+kitty = Cat.new("Kitty", "cake", 6).meow
