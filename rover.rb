1b4ae740c25d5343c6111d7566ac8884cc05c6d0
Althea Yi <altheaserena.yi@gmail.com>
2016-06-08 17:23:47 -0400

    Started rover

---
+++ rover.rb
@@ -0,0 +1,15 @@
+class Rover
+  def initialize(location, direction)
+    @location = location
+    @direction = direction
+
+  def read_instruction
+  end
+
+  def move
+  end
+
+  def turn
+  end 
+
+end
